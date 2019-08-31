dad511b2459a5d6305d798c1d247575a4edba311
DraciVik <viktor.stojanov90@gmail.com>
2019-08-31 19:06:35 +0200

    Added pseudo code for merge sort

---
+++ merge-sort.js
@@ -0,0 +1,8 @@
+// PSEUDOCODE
+
+// MergeSort (list)
+//     base case: if list.length < 2 return
+//     break the list into halves L & R
+//     Lsorted = mergeSort(L);
+//     Rsorted = mergeSort(R);
+//     return merge(Lsorted, Rsorted);
